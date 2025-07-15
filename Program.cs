5317c488cc56e3243a8582762d1807b8070ccce5
HiraSeera <giraev3@gmail.com>
2025-07-15 21:35:49 +0300

    Add files via upload
    Код для работы с классическим шифром Цезаря

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace EduCSProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string alf = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+            
+            Console.Write("Ваше сообщение: ");
+            string SMess = Console.ReadLine();
+            Console.Write("Сдвиг: ");
+            int sdvg = Convert.ToInt32(Console.ReadLine());
+            string neoMess = "";
+
+            string neoAlf = SdvgReform(alf,sdvg);
+            char[] messBuf = new char[SMess.Length];
+            
+            
+            for (int i=0; i < SMess.Length; i++)
+            {
+                if (!" ,.?-_:;!".Contains(SMess[i]))
+                {
+                    neoMess += neoAlf[alf.IndexOf(SMess[i])];
+                }
+                else
+                {
+                    neoMess += SMess[i];
+                }
+            }
+            Console.WriteLine(neoMess);
+        }
+        private static string SdvgReform(string alfavit, int sdvg)
+        {
+            string realf = "";
+            for (int j= (alfavit.Length) - sdvg; j < (alfavit.Length); j++)
+            {
+                realf += alfavit[j];
+            }
+            for (int i=0; i < alfavit.Length-sdvg; i++)
+            {
+                realf += alfavit[i];
+            }
+            
+            return realf;
+        }
+    }
+}
